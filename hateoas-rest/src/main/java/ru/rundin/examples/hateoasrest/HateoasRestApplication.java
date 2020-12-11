05eccf2cd6913e717aefd2f21eb01cfb7d721b61
Alexey Rundin <alexey.rundin@gmail.com>
2020-12-11 11:11:14 +0700

    hateoas-rest example

---
+++ hateoas-rest/src/main/java/ru/rundin/examples/hateoasrest/HateoasRestApplication.java
@@ -0,0 +1,13 @@
+package ru.rundin.examples.hateoasrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HateoasRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HateoasRestApplication.class, args);
+	}
+
+}
