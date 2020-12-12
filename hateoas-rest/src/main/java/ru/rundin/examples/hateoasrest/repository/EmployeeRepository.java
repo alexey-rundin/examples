82214d5bd41a2ac1f9882348f1045b914254ff8b
Alexey Rundin <alexey.rundin@gmail.com>
2020-12-12 14:23:27 +0700

    hateoas links creation in EmployeeController

--- hateoas-rest/src/main/java/ru/rundin/examples/hateoasrest/repository/EmployeeRepository.java
+++ hateoas-rest/src/main/java/ru/rundin/examples/hateoasrest/repository/EmployeeRepository.java
@@ -1,11 +1,13 @@
 package ru.rundin.examples.hateoasrest.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.rundin.examples.hateoasrest.entity.Employee;
 
 /**
  * @author Alexey Rundin
  * @since 11.12.2020
  */
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
--- hateoas-rest/src/main/java/ru/rundin/examples/hateoasrest/web/EmployeeController.java
+++ hateoas-rest/src/main/java/ru/rundin/examples/hateoasrest/web/EmployeeController.java
@@ -1,12 +1,18 @@
 package ru.rundin.examples.hateoasrest.web;
 
 import lombok.AllArgsConstructor;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
 import ru.rundin.examples.hateoasrest.entity.Employee;
 import ru.rundin.examples.hateoasrest.error.EmployeeNotFoundException;
 import ru.rundin.examples.hateoasrest.repository.EmployeeRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 /**
  * @author Alexey Rundin
@@ -18,8 +24,14 @@ public class EmployeeController {
     private final EmployeeRepository employeeRepository;
 
     @GetMapping("employees")
-    public List<Employee> all() {
-        return employeeRepository.findAll();
+    public CollectionModel<EntityModel<Employee>> all() {
+        List<EntityModel<Employee>> employees = employeeRepository.findAll().stream()
+                .map(employee -> EntityModel.of(employee,
+                        linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+                        linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
     }
 
     @PostMapping("/employees")
@@ -28,8 +40,11 @@ public class EmployeeController {
     }
 
     @GetMapping("/employees/{id}")
-    public Employee one(@PathVariable Long id) {
-        return employeeRepository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+    public EntityModel<Employee> one(@PathVariable Long id) {
+        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+        return EntityModel.of(employee,
+                linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
     }
 
     @PutMapping("/employees/{id}")
